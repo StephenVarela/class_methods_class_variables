6bab72c3f1c02796dfb95af8dd3ffd67c26cf2dd
StephenVarela <varelast92@gmail.com>
2018-09-11 18:40:13 -0400

    Finished Zombie apocalypse

--- Zombie_Apocalypse.rb
+++ Zombie_Apocalypse.rb
@@ -30,11 +30,12 @@ class Zombie
     if(outrun_zombie?(your_speed))
       puts "You escaped unscathe!"
     elsif (survive_attack?(your_strength))
-      puts "You beat the zombie but it bit you so you became a zombie"
+      puts "You are now a zombie"
+      @@horde << Zombie.new(rand(@@max_speed+1), rand(@@max_strength+1))
+      puts
     else
       puts "You died"
     end
-  
   end
 
   def outrun_zombie?(speed)
@@ -84,13 +85,32 @@ class Zombie
 
 end
 
-puts"-----"
-Zombie.new_day()
-p Zombie.all().length
-puts"-----"
-Zombie.new_day()
-p Zombie.all().length
-puts"-----"
-Zombie.new_day()
-p Zombie.all().length
-puts"-----"
+
+
+puts "---------------Starting Game---------------------"
+Zombie.new_day
+day1 = Zombie.all
+puts "There are #{day1.size} zombies at the start of day 1"
+day1.each do |zombie|
+  puts zombie.encounter
+end
+end_day1 = Zombie.all
+puts "There are #{end_day1.size} zombies at the end of day 1"
+
+puts "---------------End Game---------------------"
+
+#
+# zombie1 = Zombie.all[0]
+# zombie2 = Zombie.all[1]
+# zombie3 = Zombie.all[2]
+# puts zombie1.encounter # You are now a zombie
+# puts zombie2.encounter # You died
+# puts zombie3.encounter # You died
+# Zombie.new_day
+# puts Zombie.all.inspect # [#<Zombie:0x005626ecc5e1f8 @speed=0, @strength=0>, #<Zombie:0x005626ecc5e180 @speed=3, @strength=3>, #<Zombie:0x005626ecc5e158 @speed=1, @strength=2>, #<Zombie:0x005626ecc5e090 @speed=0, @strength=4>]
+# zombie1 = Zombie.all[0]
+# zombie2 = Zombie.all[1]
+# zombie3 = Zombie.all[2]
+# puts zombie1.encounter # You got away
+# puts zombie2.encounter # You are now a zombie
+# puts zombie3.encounter # You died
